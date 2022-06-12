a531009326bf99f54285213fc2a868485b743fe9
cisco-ramon-cis <shinesackiteyteye@gmail.com>
2022-06-12 17:54:41 +0000

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-ReactDOM.render(
-  <h1>Hello, React!</h1>,
-  document.getElementById('root')
-);
+let counter = 0;
+function show() {
+  counter++;
+  const el = <p>{counter}</p>;
+  ReactDOM.render(el, document.getElementById('root'));
+}
+
+setInterval(show, 1000);
